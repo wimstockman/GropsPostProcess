2296ad5e557659530fcc23f770160703e58e3b70
wimstockman <wim.stockman@gmail.com>
2021-04-30 16:51:03 +0200

    Update GropsLineCompress.sh

--- GropsLineCompress.sh
+++ GropsLineCompress.sh
@@ -3,38 +3,81 @@
 # Inspired by Werner Lemberg response on 2021-01-21 in the gnu groff mailinglist subject "Huge Filesize of PS file 12M" 
 # This script compresses the multiple consecutive DL (drawline) statements grops output to draw a single line to one DL statement
 # Usage: GropsLineCompress.sh filename > outputfile
-
 awk '/ DL/{ORS=" ";print $0; ORS=RS ; next;}{print $0}' $1 | sed 's/ DL/ DL\n/g' | \
 awk '
 BEGIN {
-	e = 0}
+	e = 0
+}
 {
-	if ($NF == "DL"){
-		if (stacking==1){
-			line[e-1]=$(NF-1)
-			line[e-2]=$(NF-2)
+	if( $NF == "DL"){
+		if (stacking == 1) {
+			if ( lijnmode == "H") {
+			   if ((x1 == $(NF-2)) && (y1 == $(NF-3)) && (y2 == $(NF-1))) {
+				line[e-4] = $(NF-4);
+				x1 = $(NF-4)
+				x2 = $(NF-2)
+				}
+			   else {
+				for (i=1;i<=e;i++){
+					str=str" "line[i]
+				}
+				print str
+				str=""
+				lijnmode=""
+				for(k in line){delete line[k]}
+				stacking=0;
+				}
+			 }
+			if ( lijnmode == "V") {
+			   if ((y1 == $(NF-1)) && (x1 == $(NF-4)) && (x2 == $(NF-2))) {
+				line[e-3] = $(NF-3);
+				y1 = $(NF-3)
+				y2 = $(NF-1)
+				}
+			   else {
+				for (i=1;i<=e;i++){
+					str=str" "line[i]
+				}
+				print str
+				str=""
+				lijnmode=""
+				for(k in line){delete line[k]}
+				stacking=0;
+				}
 			}
-		else {
-			for(i=NF;i>0;i--){
+		}
+		if (stacking == 0) {
+			for(i=NF;i>0;i--) {
 				line[i]=$i
 			}
-			stacking = 1
-			e = NF
+			stacking = 1;
+			e = NF;
+			x1 = line[e-4]
+			x2 = line[e-2]
+			y1 = line[e-3]
+			y2 = line[e-1]
+			if( x1 == x2) lijnmode = "V";
+			if( y1 == y2) {
+				lijnmode = "H";
+				print "horiz " x1 " " x2 " " y1 " " y2 > "/dev/stderr"
+			}
 		}
 	}
 	else {
-		if (stacking==1){
+		if(stacking == 1){
 			for (i=1;i<=e;i++){
 				str=str" "line[i]
 			}
-			print str 
+			print str
 			str=""
-			for (k in line){delete line[k]}
+			lijnmode=""
+			for(k in line){delete line[k]}
 			stacking=0
 			print $0
 		}
-		else{
-			print $0
+		else {
+			print $0;
 		}
 	}
-}'
+}
+'
